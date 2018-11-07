750d889969f6cd5cd0fe5dfa9b4b02e6375343e8
janetmugogo <janetmugogo@gmail.com>
2018-11-07 22:56:45 +0300

    [start #161742928]start get all parcels

--- app/api/v1/views/parcel_views.py
+++ app/api/v1/views/parcel_views.py
@@ -1,8 +1,10 @@
 from flask_restful import Resource, Api, reqparse
-from ..models.parcel_model import Parcels
-from flask import request,jsonify,make_response
+from app.api.v1.models.parcel_model import Parcels
+from flask import request, jsonify, make_response
 
 db = Parcels()
+
+
 class Parcels(Resource):
     def post(self):
         data = request.get_json(force=True)
@@ -13,8 +15,8 @@ class Parcels(Resource):
         address = data['address']
         weight = data['weight']
 
-        db.save_parcel(name,phonenumber,idnumber,location,address,weight)
-        return make_response(jsonify({"message":"order created successfully"}))
+        db.save_parcel(name, phonenumber, idnumber, location, address, weight)
+        return make_response(jsonify({"message": "order created successfully"}))
 
-
-
+    def get(self):
+        return db.get_all_parcels()
