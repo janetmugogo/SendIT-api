1de445a93af4408965317e606974a6e62ca88932
janetmugogo <janetmugogo@gmail.com>
2018-11-07 18:20:55 +0300

    [finish #161781103]finish chore create models

---
+++ app/api/v1/models/parcel_model.py
@@ -0,0 +1,53 @@
+parcels = []
+
+
+class Parcels:
+    def __init__(self):
+        self.db = parcels
+
+    def save_parcel(self, name, phonenumber, idnumber, location, address, weight):
+        new_order = {
+            'order_id': len(self.db) + 1,
+            'name': name,
+            'phonenumber': int(phonenumber),
+            'idnumber': idnumber,
+            'location': location,
+            'address': address,
+            'weight': int(weight),
+            'status':"intransit",
+            'destination':"nairobi"
+        }
+        self.db.append(new_order)
+
+    def get_all_parcels(self):
+
+        return self.db
+
+    def get_single_order(self, order_id):
+        for order in self.db:
+            if order['order_id'] == order_id:
+                return order
+    def cancel_order(self, order_id):
+        for order in self.db:
+            if order['order_id'] == order_id:
+                order['status'] = "cancelled"
+
+    # def change_destination(self, order_id):
+    #     for order in self.db:
+    #         if order['order_id'] == order_id and order['status'] == "intransit":
+    #             order['destination'] = "mombasa"
+
+
+
+
+
+# janet = Parcels()
+# janet.save_parcel('book', '987654321', '345678', 'kisumu', '31156', '2')
+# janet.save_parcel('kitabu', '987654321', '345678', 'kitale', '31156', '3')
+# print(janet.db)
+
+# print(janet.get_single_order(2))
+# print(janet.cancel_order(1))
+# print(janet.get_all_parcels())
+# print(janet.change_destination(1))
+
