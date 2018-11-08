6919d4aac12d8525e78322d2fd9006f862de6bb7
janetmugogo <janetmugogo@gmail.com>
2018-11-08 19:38:53 +0300

    [Start #161815477]started creating test parcels

---
+++ app/tests/test_parcels.py
@@ -0,0 +1,12 @@
+import unittest
+import json
+from .base_test import BaseTest
+class TestParcel(BaseTest)
+
+    def test_create_order(self):
+        resp=self.client.post('/api/v1/parcels',data = json.dumps(self.test_parcel), content_type='application/json')
+        self.assertEqual(resp.status_code,200)
+
+    def test_get_order(self):
+        resp = self.client.get('/api/v1/parcels')
+        self.assertEqual(resp.status_code,200)
