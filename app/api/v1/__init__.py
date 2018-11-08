cb3ee333a4d9f86a31632d5c4de2a53d20282cd0
janetmugogo <janetmugogo@gmail.com>
2018-11-08 11:32:22 +0300

    [finish #161797192]finished editing inside

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,9 +2,11 @@ from flask import Blueprint
 from flask_restful import Api
 
 from .views.parcel_views import Parcels
+from .views.user_views import users
 
 version1 = Blueprint('apiv1', __name__, url_prefix='/api/v1')
 api = Api(version1)
 
 
 api.add_resource(Parcels, '/parcels')
+api.add_resource(Users, '/users')
--- app/api/v1/models/user_model.py
+++ app/api/v1/models/user_model.py
@@ -4,23 +4,21 @@ users = []
 class Users:
     def __init__(self):
         self.db = users
+    def save_user(self, username,email, password, confirm_password):
+        new_user = {
+            'user_id': len(self.db) + 1,
+            'username': username,
+            'email': email,
+            'password': password,
+            'confirm_password': confirm_password,
+            # 'status': "intransit"
+        }
+        self.db.append(new_user)
 
+    def get_all_parcels(self):
+        return self.db
 
-"""user can create an account and their details saved"""
-
-
-def save_user(self, username, email, password, confirm_password):
-    new_user = {
-        'user_id': len(self.db) + 1,
-        'username': username,
-        'email': email,
-        'password': password,
-        'confirm_password': confirm_password,
-        'status': "intransit"
-    }
-    self.db.append(new_user)
-
-# janet = Users()
-# janet.save_user('denzel', 'denzelkanyi@gmail.com', '123', '123')
-#
-# print(janet.db)
+janet = Users()
+janet.save_user('denzel', 'denzelkanyi@gmail.com', '123', '123')
+print(janet.get_all_parcels())
+print(janet.db)
