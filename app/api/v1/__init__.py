2e3909f220734b239270fce9b70be0fcc559df9c
janetmugogo <janetmugogo@gmail.com>
2018-11-08 18:46:09 +0300

    [finished #161743251]

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from .views.parcel_views import Parcels
+from .views.parcel_views import Parcels,Parcel,Cancel
 from .views.user_views import Users
 
 version1 = Blueprint('apiv1', __name__, url_prefix='/api/v1')
@@ -9,4 +9,6 @@ api = Api(version1)
 
 
 api.add_resource(Parcels, '/parcels')
+api.add_resource(Parcel, '/parcel<int:order_id>')
+api.add_resource(Cancel, '/parcels<int:order_id>/cancel')
 api.add_resource(Users, '/users')
---
+++ app/api/v1/views/user_views.py
@@ -0,0 +1,34 @@
+from flask_restful import Resource, Api, reqparse
+from app.api.v1.models.user_model import Users
+from flask import request, jsonify, make_response
+
+db = Users()
+
+
+class Users(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'username', type=str, required=True, help='username field is required', location='json')
+
+    parser.add_argument(
+        'email', type=str, required=True, help='email field is required', location='json')
+
+    parser.add_argument(
+        'password', type=str, required=True, help='password field is required', location='json')
+
+    parser.add_argument(
+        'confirm_password', type=str, required=True, help='confirm_password field is required', location='json')
+
+
+    def post(self):
+        data = Users.parser.parse_args()
+        for input in data.values():
+            if input == "":
+                return make_response(jsonify({"error": "fields cannot be blank"})), 400
+            username = data['username']
+            email = data['email']
+            password = data['password']
+            confirm_password = data['confirm_password']
+
+            db.save_user(username, email, password, confirm_password)
+            return make_response(jsonify({"message": "user created successfully"}))
