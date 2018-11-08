d1a4529890ccac3a3f53e0895759f025ccf7efd7
janetmugogo <janetmugogo@gmail.com>
2018-11-08 14:52:29 +0300

    [finish #161743026]finish editing inside getsingleorder class

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,9 +2,17 @@ from flask import Blueprint
 from flask_restful import Api
 
 from .views.parcel_views import Parcels
+from .views.parcel_views import Parcel
+from .views.parcel_views import Cancel
+from .views.parcel_views import ChangeDestination
+
+
 
 version1 = Blueprint('apiv1', __name__, url_prefix='/api/v1')
 api = Api(version1)
 
 
 api.add_resource(Parcels, '/parcels')
+api.add_resource(Parcel, '/parcel/<int:order_id>')
+api.add_resource(Cancel, '/parcels/<int:order_id>/cancel')
+api.add_resource(ChangeDestination, '/parcels/<int:order_id>/changedestination')
--- app/api/v1/views/parcel_views.py
+++ app/api/v1/views/parcel_views.py
@@ -20,11 +20,36 @@ class Parcels(Resource):
 
     def get(self):
         """get all orders from the database"""
-        return db.get_all_parcels()
+        return db.get_all_parcels(), 200
 
 
 class Parcel(Resource):
     def get(self, order_id):
         """fetches a single order for the user/admin"""
+        order = db.get_single_order(order_id)
+        if order:
+            return order, 200
+        return make_response(jsonify({"message": "order does not exist"})), 404
 
-        return db.get_single_order(order_id)
+
+class Cancel(Resource):
+    def put(self, order_id):
+        """allows user to cancel a specific order"""
+        data = request.get_json()
+        status = data['status']
+        order = db.get_single_order(order_id)
+        if order:
+            db.cancel_order(order_id, status)
+            return {"message": "order status updated successfully"}
+        return make_response(jsonify({"message": "order does not exist"})), 404
+class ChangeDestination(Resource):
+    def put(self, order_id):
+        """allow user to change destination"""
+        data = request.get_json()
+        destination = data['destination']
+        order = db.get_single_order(order_id)
+        if order:
+            db.change_destination(order_id, destination)
+            return {"message": "destination updated successfully"}
+        return make_response(jsonify({"message": "order does not exist"})), 404
+
